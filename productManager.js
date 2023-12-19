3f67e00150b3d1919d10c50b6866f3c3de6eb4e9
Guillermo Vaamonde <guillevaamonde22@gmail.com>
2023-12-19 00:24:37 -0300

    entregaPM

--- productManager.js
+++ productManager.js
@@ -2,7 +2,7 @@ class ProductManager{
     constructor(){
         this.products = []
     }
-    static id = 0
+    static id = 1
 
     
     addProduct(title, description, price, thumbnail, code, stock) {
