d23df2b9da37957120dd0f6b1c3834855a71bab2
GuilleVaamonde <131558050+GuilleVaamonde@users.noreply.github.com>
2023-12-19 00:23:45 -0300

    Add files via upload

---
+++ productManager.js
@@ -0,0 +1,34 @@
+class ProductManager{
+    constructor(){
+        this.products = []
+    }
+    static id = 0
+
+    
+    addProduct(title, description, price, thumbnail, code, stock) {
+        ProductManager.id++
+        this.products.push({title,description,price,thumbnail,code,stock, id:ProductManager.id})
+    }
+
+    getProducts(){
+        return this.products;
+    } 
+
+    getProductsById(id){
+        if(this.products.find((producto)=>producto.id===id)){
+        alert('Id Existe')
+    } else{
+        alert('Id no Existe')}} 
+}
+
+const productos = new ProductManager
+
+/* console.log(productos.getProducts()); */
+
+productos.addProduct('Titulo1','descripcion1', 500, 'Imagen1', 'cod001', '10')
+productos.addProduct('Titulo2','descripcion2', 500, 'Imagen2', 'cod002', '10')
+productos.addProduct('Titulo3','descripcion3', 500, 'Imagen3', 'cod003', '10')
+
+console.log(productos.getProducts());
+
+productos.addProduct()
